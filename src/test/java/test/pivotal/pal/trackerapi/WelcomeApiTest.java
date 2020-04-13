24ef9166e5e749abd11c17fdba16a796e6877a31
Mageshkumar <Mageshkumar@gmail.com>
2020-04-13 17:20:13 +0530

    Assignment #2

--- src/test/java/test/pivotal/pal/trackerapi/WelcomeApiTest.java
+++ src/test/java/test/pivotal/pal/trackerapi/WelcomeApiTest.java
@@ -5,10 +5,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.context.annotation.Profile;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
+@Profile("test")
 @SpringBootTest(classes = PalTrackerApplication.class, webEnvironment = RANDOM_PORT)
 public class WelcomeApiTest {
 
